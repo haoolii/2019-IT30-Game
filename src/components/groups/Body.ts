26fc02df54720055cfa00cc30a92aa943af67cff
unnhao <unnhao@gmail.com>
2019-09-15 16:59:10 +0800

    done

--- src/components/groups/Body.ts
+++ src/components/groups/Body.ts
@@ -1,9 +1,11 @@
 import Wrapper from '@/components/elements/Wrapper'
 import WrapperContainerCenter from '@/components/elements/WrapperContainerCenter'
 import Table from '@/components/groups/Table'
-
+import Poker from '@/components/objects/Poker'
+import WaitNextBetNotify from '@/components/objects/WaitNextBetNotify'
 export default class Body extends WrapperContainerCenter {
   private _table: Wrapper
+
   constructor() {
     super()
     let rect = new PIXI.Graphics()
@@ -13,7 +15,10 @@ export default class Body extends WrapperContainerCenter {
     this._table = new Table()
     this._table.setPosition({animation: false}, this.width / 2 - 10 - this._table.width / 2, this.height / 2 - this._table.height / 2 + 60)
     this._table.setScale(false, 1.2, 1.2)
-
+    
+    let notify = new WaitNextBetNotify()
     this._centerContainer.addChild(this._table)
+    this._centerContainer.addChild(notify)
+    notify.setPosition({animation: false}, this.width /2  - 80, this.height / 2)
   }
 }
---
+++ src/config/pokerPoint.ts
@@ -0,0 +1,20 @@
+interface _pokerPoint {
+  [key: string]: number;
+}
+const pokerPoint: _pokerPoint = {
+  'A': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  '10': 0,
+  'J': 0,
+  'Q': 0,
+  'K': 0
+}
+
+export default pokerPoint
